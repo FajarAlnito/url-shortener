75c8099842c5964d0ab1062ca75c5f7f05795d93
Yudi <madewahyudi22@gmail.com>
2023-02-26 17:16:41 +0800

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      sans: ['Public Sans', ...defaultTheme.fontFamily.sans]
+    },
+  },
+  plugins: [],
+}
