230550f77faf80bb4fba43177f0d22e75d2dd75e
Yudi <madewahyudi22@gmail.com>
2023-03-03 18:40:53 +0800

    add `prettier` plugin for eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      sans: ['Public Sans', ...defaultTheme.fontFamily.sans]
+      sans: ['Public Sans', ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
